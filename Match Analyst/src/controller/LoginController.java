f179d4164fe3761f61110f455f15ac4f3217e647
Jackepepito <44113276+Jackepepito@users.noreply.github.com>
2018-10-25 17:48:57 +0200

    Neofer
    neofer

---
+++ Match Analyst/src/controller/LoginController.java
@@ -0,0 +1,22 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private AnchorPane sfondo;
+
+    @FXML
+    private Separator lineaLogin;
+
+    @FXML
+    private TextField Username;
+
+}
