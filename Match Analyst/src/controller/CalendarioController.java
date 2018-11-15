d9ecaf48d112c6360f119247d9d0c41a896250a8
stefanocapuccini <capuccini.stef@gmail.com>
2018-11-15 16:54:27 +0100

    dwdw
    dwwdfd

--- Match Analyst/src/controller/CalendarioController.java
+++ Match Analyst/src/controller/CalendarioController.java
@@ -1,17 +1,22 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
 import view.TestApp;
 import model.*;
 
-public class CalendarioController {
+public class CalendarioController implements Initializable {
 	
 	private Utente user;
 	private Database db;
@@ -19,12 +24,22 @@ public class CalendarioController {
 	@FXML
 	private Button indietro;
 	
+    @FXML
+    private ComboBox<String> partite;
+    
+    @FXML
+    private Button scegli;
+    
 	public CalendarioController(Utente user, Database db)
 	{
 		this.user = user;
 		this.db = db;
 	}
 	
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	//ResultSet rs = Database.query("SELECT count(partita.avversario) from partita");
+	
+	}
 	
 	public void indietro(ActionEvent event)
 	{
@@ -40,4 +55,9 @@ public class CalendarioController {
     		}
     }
 	
+	@FXML
+	public void scegli(ActionEvent event)
+	{
+		
+	}
 }
