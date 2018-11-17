de29e635cf57d703698638d680a77796715c348b
stefanocapuccini <capuccini.stef@gmail.com>
2018-11-17 16:02:10 +0100

    dddd
    ddddd

--- Match Analyst/src/controller/CalendarioController.java
+++ Match Analyst/src/controller/CalendarioController.java
@@ -20,56 +20,55 @@ import view.TestApp;
 import model.*;
 
 public class CalendarioController implements Initializable {
-	
+
 	private Utente user;
 	private Database db;
-	
+
 	@FXML
 	private Button indietro;
-	
-    @FXML
-    private ComboBox<String> partite;
-    
-    @FXML
-    private Label prova;
-    
-    @FXML
-    private Button scegli;
-    
 
-    @FXML
-    private Label gol_segnati;
+	@FXML
+	private ComboBox<String> partite;
 
-    @FXML
-    private Label gol_subiti;
+	@FXML
+	private Label prova;
 
-    @FXML
-    private Label marcatori;
+	@FXML
+	private Button scegli;
 
-    @FXML
-    private Label possesso_palla;
+	@FXML
+	private Label gol_segnati;
 
-    @FXML
-    private Label tiri_tot;
+	@FXML
+	private Label gol_subiti;
 
-    @FXML
-    private Label tiri_porta;
+	@FXML
+	private Label marcatori;
 
-    @FXML
-    private Label falli_commessi;
+	@FXML
+	private Label possesso_palla;
 
-    @FXML
-    private Label falli_subiti;
+	@FXML
+	private Label tiri_tot;
 
-    @FXML
-    private Label parate;
-    
+	@FXML
+	private Label tiri_porta;
+
+	@FXML
+	private Label falli_commessi;
+
+	@FXML
+	private Label falli_subiti;
+
+	@FXML
+	private Label parate;
+
 	public CalendarioController(Utente user, Database db)
 	{
 		this.user = user;
 		this.db = db;
 	}
-	
+
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		ResultSet rs = db.query("Select avversario, girone from partita");
 		try {
@@ -80,38 +79,40 @@ public class CalendarioController implements Initializable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void indietro(ActionEvent event)
 	{
 		try {
-       		HomePageController controller = new HomePageController(user, db);
-    			FXMLLoader loader = new FXMLLoader(TestApp.class.getResource("HomePage.fxml"));
-    			loader.setController(controller);
-    			ScrollPane s = (ScrollPane) loader.load();
-    			Scene scene = new Scene(s);
-    			TestApp.getStage().setScene(scene);
-    		} catch (IOException e1) {
-    			e1.printStackTrace();
-    		}
-    }
-	
+			HomePageController controller = new HomePageController(user, db);
+			FXMLLoader loader = new FXMLLoader(TestApp.class.getResource("HomePage.fxml"));
+			loader.setController(controller);
+			ScrollPane s = (ScrollPane) loader.load();
+			Scene scene = new Scene(s);
+			TestApp.getStage().setScene(scene);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
 	@FXML
 	public void scegli(ActionEvent event)
 	{
 		String[] value = partite.getValue().split(" ");
 		System.out.println(partite.getValue());
-		System.out.println(value[0]);
-		System.out.println(value[1]);
+
 		ResultSet rs = db.query("Select * from partita where avversario = '" +value[0]+ "' and girone = '" +value[1]+ "' ");
 		try {
-			System.out.println(rs.getInt("gol_segnati"));
-			System.out.println(new Integer(rs.getInt("gol_segnati")));
-			System.out.println(new Integer(rs.getInt("gol_segnati")).toString());
 			gol_segnati.setText(new Integer(rs.getInt("gol_segnati")).toString());
+			gol_subiti.setText(new Integer(rs.getInt("gol_subiti")).toString());
+			possesso_palla.setText(new Integer(rs.getInt("possesso_palla")).toString());
+			tiri_tot.setText(new Integer(rs.getInt("tiri_tot")).toString());
+			tiri_porta.setText(new Integer(rs.getInt("tiri_porta")).toString());
+			falli_commessi.setText(new Integer(rs.getInt("falli_commessi")).toString());
+			falli_subiti.setText(new Integer(rs.getInt("falli_subiti")).toString());
+			parate.setText(new Integer(rs.getInt("parate")).toString());
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
-		//gol_segnati.setText("SELECT gol_segnati from partite where avversario = '" +partite.getItems().add("avversario")+ "' and girone = '" +partite.getItems().add("avversario")+ "'");
 	}
 }
