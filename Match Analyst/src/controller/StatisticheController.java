0f15f7f44c1624aecaf84bb9a5a182a1eaadf2c0
stefanocapuccini <capuccini.stef@gmail.com>
2018-11-26 14:02:08 +0100

    sas
    asas

--- Match Analyst/src/controller/StatisticheController.java
+++ Match Analyst/src/controller/StatisticheController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -24,93 +25,101 @@ import javafx.scene.control.ScrollPane;
 import view.TestApp;
 import model.*;
 
+public class StatisticheController implements Initializable {
 
-public class StatisticheController implements Initializable{
-	
-	
 	@FXML
-	 private Database db;
-	
-	 private Utente user;
-	 
-	    @FXML
-	    private Button indietro;
+	private Database db;
 
+	private Utente user;
 
-	    @FXML
-	    private Button mostra;
-	    
-	    @FXML
-	    private ComboBox<String> andamenti;
+	@FXML
+	private Button indietro;
 
-	 
-	    @FXML
-	    private LineChart<String, Integer> grafico1;
+	@FXML
+	private Button mostra;
 
-	    @FXML
-	    private CategoryAxis x;
+	@FXML
+	private ComboBox<String> sceltaGrafico;
 
-	    @FXML
-	    private NumberAxis y;
-	    
-	    public StatisticheController(Utente user,Database db)
-	    {
-	    		this.db = db;
-	    		this.user=user;
-	    }
-	    
-	    @FXML
-	    void indietro(ActionEvent event) {
-	    	try {
-				HomePageController controller = new HomePageController(user, db);
-				FXMLLoader loader = new FXMLLoader(TestApp.class.getResource("HomePage.fxml"));
-				loader.setController(controller);
-				ScrollPane s = (ScrollPane) loader.load();
-				Scene scene = new Scene(s);
-				TestApp.getStage().setScene(scene);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-	    }
-	    
+	@FXML
+	private LineChart<String, Integer> graficoGolSegnati;
 
-	    @FXML
-	    void mostra(ActionEvent event) {
-//metodi per creare i grafici
-	    	
-	    	    
-	            if(andamenti.getValue().equals("Fase offensiva")) {
-	    		x.setLabel("Avversario");
-	    		y.setLabel("Gol");
-	    		XYChart.Series<String, Integer> series1 = new XYChart.Series<String, Integer>();
-	        	series1.setName("Gol Fatti");
-	        	ResultSet rs = db.query("SELECT gol_segnati,avversario FROM partita ");
-	        	try {
-	    			while (rs.next()){
-	    				int sum = rs.getInt("gol_segnati");
-	    				String avversario = rs.getString("avversario");
-	    				series1.getData().add(new XYChart.Data<String, Integer>(avversario, sum));				
-	    			}
-	    		}  catch (SQLException e) {
-	    			e.printStackTrace();
-	    		}
-	        	grafico1.getData().add(series1);
-	        	
-	            }
-	            
-	    	
-	    }
+	@FXML
+	private LineChart<String, Integer> graficoGolSubiti;
 
-		@Override
-		public void initialize(URL arg0, ResourceBundle arg1) {
-			
-			ObservableList<String> types = FXCollections.observableArrayList
-				    ( "Andamento generale", "Andamento in casa", "Andamento in trasferta","Fase offensiva","Fase difensiva" );
+	@FXML
+	private CategoryAxis x1;
 
-				andamenti.setItems( types );
-				
-				 
+	@FXML
+	private NumberAxis y1;
+
+	@FXML
+	private CategoryAxis x2;
+
+	@FXML
+	private NumberAxis y2;
+
+	private XYChart.Series<String, Integer> series;
+
+	public StatisticheController(Utente user, Database db) {
+		this.db = db;
+		this.user = user;
+	}
+
+	@FXML
+	void indietro(ActionEvent event) {
+		try {
+			HomePageController controller = new HomePageController(user, db);
+			FXMLLoader loader = new FXMLLoader(TestApp.class.getResource("HomePage.fxml"));
+			loader.setController(controller);
+			ScrollPane s = (ScrollPane) loader.load();
+			Scene scene = new Scene(s);
+			TestApp.getStage().setScene(scene);
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
-		
-		
+	}
+
+	@FXML
+	void mostra(ActionEvent event) {
+		graficoGolSegnati.setVisible(false);
+		graficoGolSubiti.setVisible(false);
+		if (sceltaGrafico.getValue().equals("Gol Fatti")) {
+			creaGrafico(graficoGolSegnati, x1, y1, "gol_segnati");
+		}
+
+		if (sceltaGrafico.getValue().equals("Gol Subiti")) {
+			creaGrafico(graficoGolSubiti, x2, y2, "gol_subiti");
+		}
+	}
+
+	private void creaGrafico(LineChart<String, Integer> grafico, CategoryAxis x, NumberAxis y, String s) {
+		grafico.setLegendVisible(false);
+		x.setLabel("Avversario");
+		y.setLabel("Gol");
+		series = new XYChart.Series<String, Integer>();
+		series.setName(s);
+		ResultSet rs = db.query("SELECT " + s + ", avversario FROM partita ");
+		try {
+			while (rs.next()) {
+				System.out.println(rs.getString("avversario") + " " + rs.getInt(s));
+				series.getData().add(new XYChart.Data<String, Integer>(rs.getString("avversario"), rs.getInt(s)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		grafico.getData().add(series);
+		grafico.setVisible(true);
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		// ObservableList<String> types = FXCollections.observableArrayList
+		// ("Gol Fatti", "Gol Subiti");
+
+		sceltaGrafico.getItems().add("Gol Fatti");
+		sceltaGrafico.getItems().add("Gol Subiti");
+
+	}
 }
