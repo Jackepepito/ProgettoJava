9307ae45fc57b648e855af661e4fe07ddddde834
Jackepepito <44113276+Jackepepito@users.noreply.github.com>
2018-11-30 13:30:32 +0100

    claudio

--- Match Analyst/src/controller/RosaController.java
+++ Match Analyst/src/controller/RosaController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
@@ -10,6 +11,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -106,7 +108,7 @@ public class RosaController implements Initializable{
 			try {
 				ResultSet rs;
 				rs = db.query("SELECT * FROM giocatore U where U.numero = '" +numeroinput.getValue()+ "'");
-				
+				int i=0;
 				
 				if (rs.next()){
 					messaggio.setText("Numero non disponibile");
@@ -114,9 +116,68 @@ public class RosaController implements Initializable{
 				else {
 					System.out.println("Ok");
 					db.update("INSERT INTO Giocatore VALUES ('" +numeroinput.getValue()+ "','" + nomeinput.getText()+"','" + ruoloinput.getText() + "',0, 0, 0)");
-				
 					
+					ResultSet rs3 = db.query("Select count(*) AS TOTAL  from giocatore");
+					System.out.println("Ok2");
+					 i=rs3.getInt("total");
+					 i*=5;
+					 int k=6; //i primi 5 sono le label sopra, poi ci sono 5 nodi per riga, che sono i dati dei giocatori
+					
+						    System.out.println(k);
+							griglia.getChildren().remove(k, i+1);
+							
+					
+					griglia.setGridLinesVisible(true);
+					
+				    
+				    
+				    
+				    //reinserimento dei giocatori
+				    i=2;
+					
+			        ResultSet rs2 = db.query("Select numero,nome,ruolo,gol,assist from giocatore");
+					try {
+						
+						while(rs.next()) {
+							
+							Label port4 = new Label();
+				    		port4.setText(new Integer(rs2.getInt("numero")).toString());
+				    		GridPane.setConstraints(port4, 0, i);
+				    		griglia.getChildren().add(port4);
+				    		
+							Label port = new Label();
+				    		port.setText(rs2.getString("nome").toString());
+				    		GridPane.setConstraints(port, 1, i);
+				    		griglia.getChildren().add(port);
+				    		
+				    		
+				    		Label port2 = new Label();
+				    		port2.setText(rs2.getString("ruolo").toString());
+				    		GridPane.setConstraints(port2, 2, i);
+				    		griglia.getChildren().add(port2);
+				    		
+				    		Label port3 = new Label();
+				    		port3.setText(new Integer(rs2.getInt("gol")).toString());
+				    		GridPane.setConstraints(port3, 3, i);
+				    		griglia.getChildren().add(port3);
+				    		
+				    		Label port5 = new Label();
+				    		port5.setText(new Integer(rs2.getInt("assist")).toString());
+				    		GridPane.setConstraints(port5, 4, i);
+				    		griglia.getChildren().add(port5);
+				    		
+				    		
+				    		++i;}
+						   griglia.setGridLinesVisible(true);
+					} catch (SQLException e) {
+						e.printStackTrace();
+							
+				
 				}
+					    }
+					  
+					
+				
 				
 			}	
 			
@@ -125,7 +186,7 @@ public class RosaController implements Initializable{
 			}
 		}
 			
-		messaggio.setVisible(true);
+		
 		
 }
 
@@ -174,6 +235,8 @@ public class RosaController implements Initializable{
 	    		port5.setText(new Integer(rs.getInt("assist")).toString());
 	    		GridPane.setConstraints(port5, 4, i);
 	    		griglia.getChildren().add(port5);
+	    		
+	    		
 	    		++i;}
 		} catch (SQLException e) {
 			e.printStackTrace();
